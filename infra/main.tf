e1124711300aa89c31a56af5b09f4e2d441d92ca
mvgv <mvgv1989@gmail.com>
2023-11-02 22:21:03 -0300

    fix: ajustando subnets do eks

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,15 @@
 provider "aws" {
-  region = "us-east-1" # Substitua pela sua região preferida
+  region = "us-east-1" # Replace with your preferred region
+}
+
+data "aws_ami" "eks_worker" {
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-v*"]
+  }
+
+  most_recent = true
+  owners      = ["602401143452"] # Amazon EKS AMI account ID
 }
 
 resource "aws_eks_cluster" "my_cluster" {
@@ -7,7 +17,7 @@ resource "aws_eks_cluster" "my_cluster" {
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = aws_subnet.my_subnets[].id
+    subnet_ids = aws_subnet.my_subnets[*].id
   }
 }
 
@@ -34,7 +44,7 @@ resource "aws_subnet" "my_subnets" {
   count = 2
 
   vpc_id                  = aws_vpc.my_vpc.id
-  availability_zone       = element(["us-west-2a", "us-west-2b"], count.index)
+  availability_zone       = element(["us-east-1a", "us-east-2b"], count.index)
   cidr_block              = element(["10.0.1.0/24", "10.0.2.0/24"], count.index)
   map_public_ip_on_launch = true
 }
@@ -47,10 +57,10 @@ resource "aws_eks_fargate_profile" "my_fargate_profile" {
   cluster_name = aws_eks_cluster.my_cluster.name
   fargate_profile_name = "my-fargate-profile"
   pod_execution_role_arn = aws_iam_role.fargate_execution_role.arn
-  subnet_ids = aws_subnet.my_subnets[].id
+  subnet_ids = aws_subnet.my_subnets[*].id
 
   selector {
-    namespace = "default" # Namespace do Kubernetes
+    namespace = "default" # Kubernetes Namespace
   }
 }
 
