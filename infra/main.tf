818047c0a58cd4286b6c9f570a2c075c4648c98d
mvgv <mvgv1989@gmail.com>
2023-11-05 22:02:54 -0300

    fix: refazendo o cluster eks/fargate

--- infra/main.tf
+++ infra/main.tf
@@ -33,6 +33,28 @@ module "eks" {
  }
 }
 
+resource "aws_iam_role" "fargate_role" {
+ name = "eks-fargate-role"
+
+ assume_role_policy = jsonencode({
+   Version = "2012-10-17"
+   Statement = [
+     {
+       Action = "sts:AssumeRole"
+       Effect = "Allow"
+       Principal = {
+         Service = "eks-fargate-pods.amazonaws.com"
+       }
+     },
+   ]
+ })
+}
+
+resource "aws_iam_role_policy_attachment" "fargate_policy_attachment" {
+ role       = aws_iam_role.fargate_role.name
+ policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+}
+
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
  role      = "arn:aws:iam::101478099523:role/deploy_lambda_dynamo"
  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
@@ -46,7 +68,7 @@ resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
 resource "aws_eks_fargate_profile" "my_fargate_profile" {
  cluster_name          = module.eks.cluster_name
  fargate_profile_name  = "my-fargate-profile"
- pod_execution_role_arn = module.eks.fargate_execution_role_arn
+ pod_execution_role_arn = aws_iam_role.fargate_role.arn
  subnet_ids            = module.vpc.private_subnets
 
  selector {
